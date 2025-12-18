9c1bd3b1fc60069c17b120606aded98030f9ae44
floyzg <floyzg@gmail.com>
2025-12-18 17:15:58 +0400

    fix: fix filter correct

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,23 +1,42 @@
 export function initFiltering(elements) {
   const updateIndexes = (elements, indexes) => {
     Object.keys(indexes).forEach((elementName) => {
-      if (!elements[elementName]) return;
+      const select = elements[elementName];
+      if (!select) return;
 
-      elements[elementName].replaceChildren(
-        ...Object.values(indexes[elementName]).map((name) => {
-          const el = document.createElement("option");
-          el.textContent = name;
-          el.value = name;
-          return el;
-        })
-      );
+      // сохраняем первый option (пустой)
+      const emptyOption = select.querySelector("option[value='']");
+
+      // очищаем селект
+      select.replaceChildren();
+
+      // возвращаем пустой option первым
+      if (emptyOption) {
+        select.append(emptyOption);
+      } else {
+        const empty = document.createElement("option");
+        empty.value = "";
+        empty.textContent = "";
+        select.append(empty);
+      }
+
+      // добавляем остальные варианты
+      Object.values(indexes[elementName]).forEach((name) => {
+        const option = document.createElement("option");
+        option.value = name;
+        option.textContent = name;
+        select.append(option);
+      });
+
+      // гарантируем пустое значение по умолчанию
+      select.value = "";
     });
   };
 
   const applyFiltering = (query, state, action) => {
     // очистка поля
-    if (action && action.name === "clear") {
-      const field = action.dataset.field;
+    if (action?.name === "clear") {
+      const field = action.dataset?.field;
       if (field && elements) {
         const input =
           elements[`searchBy${field[0].toUpperCase()}${field.slice(1)}`];
@@ -27,22 +46,15 @@ export function initFiltering(elements) {
 
     const filter = {};
 
-    const FILTER_FIELDS = ["seller", "customer", "date", "total"];
+    const FILTER_FIELDS = ["seller", "customer", "date", "total", "from", "to"];
 
-    const applyFiltering = (query, state, action) => {
-      const filter = {};
+    FILTER_FIELDS.forEach((field) => {
+      if (state[field]) {
+        filter[`filter[${field}]`] = state[field];
+      }
+    });
 
-      FILTER_FIELDS.forEach((field) => {
-        if (state[field]) {
-          filter[`filter[${field}]`] = state[field];
-        }
-      });
-
-      return Object.keys(filter).length ? { ...query, ...filter } : query;
-    };
-    return Object.keys(filter).length
-      ? Object.assign({}, query, filter)
-      : query;
+    return Object.keys(filter).length ? { ...query, ...filter } : query;
   };
 
   return { updateIndexes, applyFiltering };
